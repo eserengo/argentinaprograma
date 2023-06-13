1829c2d7cb5dfc19fb038a4f602a099a26abed84
eserengo <eserengo@gmail.com>
2023-06-13 09:21:52 -0300

    Agrego archivo miNombreCompleto.js

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,5 @@
+let nombre = 'Carlos Federico';
+let apellido = 'Borzani';
+let edad = 45;
+
+console.log('Hola,' + ' ' + nombre + ' ' + apellido + '.' + ' ' + 'Tienes' + ' ' + edad + ' ' + 'años.');
