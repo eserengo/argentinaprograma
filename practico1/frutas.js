79e6bbf76d017a8269fe0d3785b6ea47d832a988
eserengo <eserengo@gmail.com>
2023-06-13 10:51:06 -0300

    Agrego archivo frutas.js - practico1 terminado

---
+++ practico1/frutas.js
@@ -0,0 +1,11 @@
+let frutas = [];
+
+frutas.push('manzana', 'banana', 'pera', 'naranja', 'limon');
+
+let ultima = frutas[frutas.length-1];
+
+console.log('La ultima fruta agregada fue: %s', ultima);
+
+let primera = frutas[0];
+
+console.log('La primera fruta agregada fue: %s', primera);
