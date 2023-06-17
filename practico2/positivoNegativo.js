0ceb2cf2c7b0dc86a2022497e901a9a44ca8f9a1
eserengo <eserengo@gmail.com>
2023-06-17 10:24:26 -0300

    Agrego actualizacion a positivoNegativo.js

--- practico2/positivoNegativo.js
+++ practico2/positivoNegativo.js
@@ -1,8 +1,6 @@
 const readlineSync = require('readline-sync');
 const numero = parseInt(readlineSync.question('Ingrese un numero: '));
 
-console.log(typeof (numero));
-
 if (numero > 0) {
   console.log('El numero es positivo');
 } else if (numero < 0) {
